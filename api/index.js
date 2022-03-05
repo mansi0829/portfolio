109c96925c47368bb309bfbf64e576b2767bc16b
mansi0829 <mansigundre1@gmail.com>
2022-03-05 16:07:51 +0530

    post crud done

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ let connection = require("./db/connection");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
 let app = express();
 
 dotenv.config();
@@ -12,6 +13,7 @@ connection();
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
 
 app.get("/", (req, res) => {
   res.send("hello world");
--- api/models/Category.js
+++ api/models/Category.js
@@ -1,12 +1,10 @@
 const mongoose = require("mongoose");
 
-const categorySchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
   },
-);
+});
 
 module.exports = mongoose.model("Category", categorySchema);
